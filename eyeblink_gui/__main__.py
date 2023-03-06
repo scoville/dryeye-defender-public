787212795a795b1b7b511118f73746fbe04f9e49
fedexman <felix@rabadan.fr>
2023-03-06 23:49:13 +0100

    feat(main):add graph of blink adn update gui

--- eyeblink_gui/__main__.py
+++ eyeblink_gui/__main__.py
@@ -1,22 +1,62 @@
+from eyeblink_gui.utils.eyeblink_verification import compute_single_frame, lack_of_blink_detection
+from blinkdetector.models.heatmapmodel import load_keypoint_model
+from retinaface import RetinaFace
+import torch
 import os
 import sys
 import time
+import numpy as np
 
 
 import cv2
 from PySide6.QtCore import Qt, QThread, Signal, Slot, QTimer
-from PySide6.QtGui import QAction, QImage, QKeySequence, QPixmap
+from PySide6.QtGui import QAction, QImage, QKeySequence, QPixmap, QPainter
 from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox,
                                QHBoxLayout, QLabel, QMainWindow, QPushButton,
                                QSizePolicy, QVBoxLayout, QWidget, QGridLayout,
                                QMessageBox, QSlider)
+from PySide6.QtCharts import QBarSeries, QLineSeries, QChart, QBarSet, QValueAxis, QChartView
 
-import cv2
-import torch
-from retinaface import RetinaFace
 
-from blinkdetector.models.heatmapmodel import load_keypoint_model
-from eyeblink_gui.utils.eyeblink_verification import compute_single_frame, lack_of_blink_detection
+class BlinkGraph(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.series = QBarSeries()
+        self.blink_bar = QBarSet("blink")
+        self.chart = QChart()
+        self.chart.addSeries(self.series)
+        self.chart.setTitle("How many blink per minutes")
+        self.chart.setAnimationOptions(QChart.SeriesAnimations)
+        self.chart.createDefaultAxes()
+
+        self.chart.legend().setVisible(True)
+        self.chart.legend().setAlignment(Qt.AlignBottom)
+
+        self.chart_view = QChartView(self.chart)
+        self.chart_view.setRenderHint(QPainter.Antialiasing)
+
+        self.main_layout = QGridLayout()
+        self.main_layout.addWidget(self.chart_view, 0, 0, 3, 6)
+        self.setLayout(self.main_layout)
+
+    @Slot(list)
+    def update_graph(self, blink_history):
+        print("updating graph")
+        current_time = time.time()
+        n_minutes = ((int(current_time) - int(blink_history[0][0]))//60)
+        blink_per_minutes = [0]+[0]*n_minutes
+        for time_code, blink_value in reversed(blink_history):
+            current_minute = (int(current_time) - int(time_code))//60
+            if blink_value == 1:
+                blink_per_minutes[current_minute] += 1
+        blink_per_minutes.reverse()
+        print(f"{blink_per_minutes=}")
+        self.blink_bar.append(blink_per_minutes)
+        self.series.append(self.blink_bar)
+        self.chart.createDefaultAxes()
+        axis_y = self.chart.axes(Qt.Vertical)[0]
+        axis_y.setRange(0, max(blink_per_minutes))
 
 
 class EyeblinkModelThread(QThread):
@@ -36,24 +76,10 @@ class EyeblinkModelThread(QThread):
 
     def run(self):
         time_start = time.time()
-        # id_after = root.after(slider_value, compute_test_camera)
-        # print(slider_value)
-        # global last_alert
         blink_value = compute_single_frame(self.face_detector,
                                            self.keypoint_model, self.cap, self.DEVICE)
 
         self.update_label_output.emit(blink_value)
-        # sys.exit(-1)
-        # print(len(blink_history), blink_value)
-        # blink_history.append((time.time(), blink_value))
-        # if (time.time() - last_alert > 20) and lack_of_blink_detection(blink_history):
-        #     last_alert = time.time()
-        #     root.after_cancel(id_after)
-        #     messagebox.showwarning(title="Lack of blink", message="You need to blink")
-        #     id_after = root.after(10000, compute_test_camera)  # stop computing for 10s
-
-        # output_model.set(str(blink_value))
-        # print("time to process"+str(time.time()-time_start))
 
 
 class Window(QWidget):
@@ -63,12 +89,12 @@ class Window(QWidget):
         self.setWindowTitle("Eyeblink detection")
         self.setGeometry(0, 0, 800, 500)
         self.window_layout = QGridLayout(self)
-        self.window_layout.addWidget(self.create_settings())
-        self.pushButton = QPushButton(("Compute one frame"))
-        self.pushButton.clicked.connect(self.start)
+        self.window_layout.addWidget(self.create_settings(), 1, 0, 2, 6)
+        self.compute_button = QPushButton(("Compute one frame"))
+        self.compute_button.clicked.connect(self.start)
         self.label_output = QLabel("0")
-        self.window_layout.addWidget(self.pushButton)
-        self.window_layout.addWidget(self.label_output)
+        self.window_layout.addWidget(self.compute_button, 0, 0, 1, 1)
+        self.window_layout.addWidget(self.label_output, 0, 1, 1, 1)
 
         self.th = EyeblinkModelThread(self)
         self.th.finished.connect(self.finished)
@@ -76,7 +102,7 @@ class Window(QWidget):
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.start)
-        # self.timer.setInterval(500)
+        self.timer.setInterval(500)
 
         self.blink_history = []
         self.blink_messagebox = QMessageBox()
@@ -84,13 +110,23 @@ class Window(QWidget):
         self.blink_messagebox.setText("You didn't blink in the last 10 secondes")
         self.blink_messagebox.setInformativeText("Blink now to close the window!")
 
+        self.blink_graph = BlinkGraph()
+        self.get_stats = QPushButton(("Update statistics"))
+        self.get_stats.clicked.connect(lambda: self.blink_graph.update_graph(self.blink_history))
+        self.window_layout.addWidget(self.get_stats, 3, 0, 1, 6)
+        self.window_layout.addWidget(self.blink_graph, 4, 0, 3, 6)
+
     def create_settings(self):
         group_box = QGroupBox(("&Settings"))
-        self.toggleButton = QPushButton(("&Enable blinking detection"))
-        self.toggleButton.setCheckable(True)
-        self.toggleButton.setChecked(False)
+        self.toggle_label = QLabel(("Enable blinking detection"))
+        # self.toggle_label.adjustSize()
+        self.toggle_button = QPushButton()
+        self.toggle_button.setText("Disabled")
+        self.toggle_button.setCheckable(True)
+        self.toggle_button.setChecked(False)
         # toggleButton.setEnabled(True)
-        self.toggleButton.clicked.connect(self.set_timer)
+        self.toggle_button.clicked.connect(self.set_timer)
+        self.frequency_label = QLabel("Interval of the blinking detection")
         self.frequency_slider = QSlider(Qt.Horizontal)
         self.frequency_slider.setSingleStep(50)
         self.frequency_slider.setTickInterval(50)
@@ -98,45 +134,49 @@ class Window(QWidget):
         self.frequency_slider.setTickPosition(QSlider.TicksBothSides)
         self.frequency_slider.setValue(500)
         self.frequency_slider.valueChanged.connect(self.set_timer_interval)
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.toggleButton)
-        vbox.addWidget(self.frequency_slider)
-        vbox.addStretch(1)
+        vbox = QGridLayout()
+        vbox.addWidget(self.toggle_label, 0, 0, 1, 1)
+        vbox.addWidget(self.toggle_button, 0, 1, 1, 1)
+        vbox.addWidget(self.frequency_label, 1, 0, 1, 1)
+        vbox.addWidget(self.frequency_slider, 1, 1, 1, 3)
+        # vbox.addStretch(1)
         group_box.setLayout(vbox)
         return group_box
 
-    @Slot()
+    @ Slot()
     def start(self):
         print("starting thread for computing one frame")
         self.th.start()
 
-    @Slot()
+    @ Slot()
     def finished(self):
         print("Thread is finished")
         lack_blink = lack_of_blink_detection(self.blink_history)
         if lack_blink:
             self.blink_messagebox.exec()
 
-    @Slot(int)
+    @ Slot(int)
     def output_slot(self, output):
         self.label_output.setText(str(output))
         self.blink_history.append((time.time(), output))
         if output == 1:
             self.blink_messagebox.close()
 
-    @Slot()
+    @ Slot()
     def set_timer_interval(self, slider_value):
         self.timer.setInterval(slider_value)
         print(f"{slider_value=}")
 
-    @Slot()
+    @ Slot()
     def set_timer(self):
         print("timer check")
-        button_state = self.toggleButton.isChecked()
+        button_state = self.toggle_button.isChecked()
         if button_state:
+            self.toggle_button.setText("Enabled")
             self.timer.start()
             print("timer started")
         else:
+            self.toggle_button.setText("Disabled")
             print("timer stop")
             self.timer.stop()
 
