d4aa3840fcae8d3e62d78770890f71b0c459761a
Félix <46397982+fedexman@users.noreply.github.com>
2023-04-14 10:38:04 +0900

    chore(setup.py):add image to files dependencies

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ build_options = {'packages': [], 'excludes': [], 'include_files':[
     'submodules/eyeblink-detection/assets/face-detection-adas-0001/FP16-INT8/face-detection-adas-0001.xml',
     'submodules/eyeblink-detection/assets/vino_preprocess/lmks.bin',
     'submodules/eyeblink-detection/assets/vino_preprocess/lmks.xml',
+    'images/blink.png'
     ]}
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('eyeblink_gui/__main__.py', base=base, target_name = 'eyeblink_gui')
+    Executable('eyeblink_gui/__main__.py', base=base, target_name = 'eyeblinkgui')
 ]
 
 setup(name='eyeblink_gui',
