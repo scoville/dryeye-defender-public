6da878a7f13480201d8fccfed0b005e0217be5f1
Félix <46397982+fedexman@users.noreply.github.com>
2023-04-14 12:24:44 +0900

    refactor(pyprojet,setup):move config to pyproject

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
+import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-build_options = {'packages': [], 'excludes': [], 'include_files':[
-    'submodules/eyeblink-detection/assets/face-detection-adas-0001/FP16-INT8/face-detection-adas-0001.bin',
-    'submodules/eyeblink-detection/assets/face-detection-adas-0001/FP16-INT8/face-detection-adas-0001.xml',
-    'submodules/eyeblink-detection/assets/vino_preprocess/lmks.bin',
-    'submodules/eyeblink-detection/assets/vino_preprocess/lmks.xml',
-    'images/blink.png'
-    ]}
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('eyeblink_gui/__main__.py', base=base, target_name = 'eyeblinkgui')
+    Executable('eyeblink_gui/__main__.py', base=base, target_name='eyeblinkgui')
 ]
 
-setup(name='eyeblink_gui',
-      version = '1.0',
-      description = 'Prototype for eye health',
-      options = {'build_exe': build_options},
-      executables = executables)
+setup(version='1.0', executables=executables)
