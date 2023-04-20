1a790938ee3103c72eabf8768ace257d0a56698d
Félix <46397982+fedexman@users.noreply.github.com>
2023-04-20 16:27:03 +0900

    refactor(ear_graph): docs and renaming variable

--- eyeblink_gui/widgets/ear_graph.py
+++ eyeblink_gui/widgets/ear_graph.py
@@ -12,50 +12,62 @@ LOGGER = logging.getLogger(__name__)
 
 
 class EarGraph(QChart):
-    def __init__(self, thread=EyeblinkModelThread, parent=None):
-        super().__init__(QChart.ChartTypeCartesian,  parent, Qt.WindowFlags())
+    def __init__(self, thread: EyeblinkModelThread, parent=None):
+        """Create the graph with the two series for left and rigth eye
+
+        :param thread: thread for connecting to signal
+        """
+        super().__init__(QChart.ChartTypeCartesian, parent, Qt.WindowFlags())
 
         # `update graph` is called each time thread emit update ear values signal
         thread.update_ear_values.connect(self.update_graph)
 
-        self._series_left = QSplineSeries(self)
-        self._series_rigth = QSplineSeries(self)
-        self._axisX = QValueAxis()
-        self._axisY = QValueAxis()
-        init_time = time.time()
-        self._x = init_time
-        self._y = 0
+        # serie for left eye
+        self.series_left = QSplineSeries(self)
+        # serie for rigth eye
+        self.series_rigth = QSplineSeries(self)
+        self.axisX = QValueAxis()
+        self.axisY = QValueAxis()
+        self.current_x = 9.5
 
+        # colors of the graph lines
         green = QPen(Qt.green)
         green.setWidth(3)
         blue = QPen(Qt.blue)
         blue.setWidth(3)
-        self._series_left.setPen(green)
-        self._series_left.append(self._x, self._y)
-        self._series_rigth.setPen(blue)
-        self._series_rigth.append(self._x, self._y)
+        self.series_left.setPen(green)
+        self.series_left.append(self.current_x, 0)
+        self.series_left.setName("Left ear")
+        self.series_rigth.setPen(blue)
+        self.series_rigth.append(self.current_x, 0)
+        self.series_rigth.setName("Rigth ear")
 
-        self.addSeries(self._series_left)
-        self.addSeries(self._series_rigth)
-        self.addAxis(self._axisX, Qt.AlignBottom)
-        self.addAxis(self._axisY, Qt.AlignLeft)
+        self.addSeries(self.series_left)
+        self.addSeries(self.series_rigth)
+        self.addAxis(self.axisX, Qt.AlignBottom)
+        self.addAxis(self.axisY, Qt.AlignLeft)
 
-        self._series_left.attachAxis(self._axisX)
-        self._series_left.attachAxis(self._axisY)
-        self._series_rigth.attachAxis(self._axisX)
-        self._series_rigth.attachAxis(self._axisY)
-        self._axisX.setTickCount(10)
-        self._axisX.setRange(init_time-8, init_time+1)
-        self._axisY.setRange(0, 1)
+        self.series_left.attachAxis(self.axisX)
+        self.series_left.attachAxis(self.axisY)
+        self.series_rigth.attachAxis(self.axisX)
+        self.series_rigth.attachAxis(self.axisY)
+        self.axisX.setTickCount(10)
+        # we put a range not center to get the last values on the rigth
+        self.axisX.setRange(0, 10)
+        # ear values are mainly between 0 and 1
+        self.axisY.setRange(0, 1)
 
     @Slot()
-    def update_graph(self, left_ear, rigth_ear, time):
-        x = (self.plotArea().width() / self._axisX.tickCount())/20
-        y = ((self._axisX.max() - self._axisX.min()) / self._axisX.tickCount())/20
-        self._x += y
-        self._y = left_ear
-        self._series_left.append(self._x, self._y)
-        self._series_rigth.append(self._x, rigth_ear)
-        # diff_time = time - self._old_x
-        # LOGGER.info("diff_time=%s", diff_time)
-        self.scroll(x, 0)
+    def update_graph(self, left_ear, rigth_ear):
+        """Update the graph with new ear values
+
+        :param left_ear: left ear value
+        :param rigth_ear: rigth ear value
+        """
+        x_scroll = (self.plotArea().width() / self.axisX.tickCount())/20
+        new_x_diff = ((self.axisX.max() - self.axisX.min()) / self.axisX.tickCount())/20
+        self.current_x += new_x_diff
+        self.series_left.append(self.current_x, left_ear)
+        self.series_rigth.append(self.current_x, rigth_ear)
+
+        self.scroll(x_scroll, 0)
