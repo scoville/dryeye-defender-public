11ed569a9fa6ddaef3c3085c201e04a0f77b38a9
Praneil Kamat <pkamat@mac.com>
2023-05-11 14:39:53 +0100

    feat(widgets/animated_blink_reminder): add AnimatedBlinkReminder class

---
+++ eyeblink_gui/widgets/animated_blink_reminder.py
@@ -0,0 +1,75 @@
+"""Class for the animated blink reminder widget"""
+import os
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QMovie
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
+
+
+class AnimatedBlinkReminder(QWidget):
+    """Widget that pops up to remind user to blink"""
+
+    def __init__(
+            self,
+            dismiss_callback,
+            duration_lack,
+            alert_seconds_cooldown,
+            width=320,
+    ):
+        """Initialize all variable and create the layout of the window
+        :param parent: parent of the widget, defaults to None
+        """
+        super().__init__()
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Set up the movie
+        path = "images/blink_animated.gif"
+        assert os.path.exists(path)
+        self.movie = QMovie(path)
+        assert self.movie.isValid(), f"{path} file invalid "
+
+        self.gif_label = QLabel("Blinking Gif")
+        self.gif_label.setMovie(self.movie)
+        self.gif_label.setFixedWidth(width)
+        self.movie.start()
+
+        self.layout.addWidget(self.gif_label)
+
+        # Set up the text
+        self.text_label = QLabel("Text")
+        self.text_label.setText(
+            f"You didn't blink in the last {duration_lack} seconds"
+        )
+        self.text_label.setFixedWidth(width)
+        self.text_label.setAlignment(Qt.AlignCenter)
+        self.text_label.setWordWrap(True)
+        self.text_label.setStyleSheet("font-weight: bold;")
+        self.layout.addWidget(self.text_label)
+
+        self.informative_text_label = QLabel("Informative Text")
+        self.informative_text_label.setText(
+            "Blink now to close the window or click 'Dismiss' to "
+            f"dismiss for {alert_seconds_cooldown} seconds"
+        )
+        self.informative_text_label.setFixedWidth(width)
+        self.informative_text_label.setAlignment(Qt.AlignCenter)
+        self.informative_text_label.setWordWrap(True)
+        self.layout.addWidget(self.informative_text_label)
+
+        # Set up the button
+        self.button = QPushButton("Dismiss")
+        self.button.setFixedWidth(width)
+        self.button.clicked.connect(
+            lambda: (
+                dismiss_callback(),
+                self.close()
+            )
+        )
+        self.layout.addWidget(self.button)
+    
+    def update_duration_lack(self, duration_lack):
+        self.text_label.setText(
+            f"You didn't blink in the last {duration_lack} seconds"
+        )
