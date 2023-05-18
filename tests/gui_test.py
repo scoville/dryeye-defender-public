fd4c3d54647f66928110062ec3cb122d3fd2b71e
Praneil Kamat <pkamat@mac.com>
2023-05-18 20:46:50 +0100

    chore(Dockerfile): test running several apt-get commands to circumvent current error

--- tests/gui_test.py
+++ tests/gui_test.py
@@ -2,9 +2,9 @@
 This is a unit test for running the application, and testing the detector on a few dummy frames.
 """
 import pytest
+from time import sleep
 
 from pytestqt import qtbot
-from pyvirtualdisplay import Display
 import pytest_xvfb
 
 from eyeblink_gui.widgets.window import Window
@@ -14,4 +14,6 @@ def test_application(qtbot):
     if not pytest_xvfb.xvfb_available():
         raise Exception("Tests need Xvfb to run.")
     widget = Window()
+    widget.show()
     qtbot.addWidget(widget)
+    sleep(5)
