a2cc78a31a3266ab48592a07f54d3a5fe4d0b643
Praneil Kamat <pkamat@mac.com>
2023-05-19 07:34:29 +0100

    test(gui_test): test computing one frame

--- tests/gui_test.py
+++ tests/gui_test.py
@@ -2,15 +2,34 @@
 This is a unit test for running the application, and testing the detector on a few dummy frames.
 """
 import pytest
-from time import sleep
+from unittest.mock import patch
 
 from pytestqt import qtbot
 import pytest_xvfb
-from PySide6.QtWidgets import QMainWindow
+import cv2
+import PySide6.QtCore as QtCore
 
-def test_application(qtbot):
+from eyeblink_gui.__main__ import MainWindow
+
+
+@pytest.fixture(autouse=True, scope='session')
+def ensure_xvfb():
     if not pytest_xvfb.xvfb_available():
         raise Exception("Tests need Xvfb to run.")
-    qmainwindow = QMainWindow()
-    qmainwindow.show()
-    qtbot.addWidget(qmainwindow)
+
+
+def test_application(qtbot):
+    # TODO: ensure that DEBUG is set to True
+    with patch("eyeblink_gui.widgets.window.EyeblinkModelThread.init_cap", new=mock_init_cap), \
+         patch("eyeblink_gui.widgets.window.get_cap_indexes", new=lambda *args, **kwargs: [0]):
+        main_window = MainWindow()
+        main_window.show()
+        qtbot.addWidget(main_window)
+        window = main_window.centralWidget()
+        for i in range(1):
+            with qtbot.waitSignal(window.eye_th.finished, timeout=3000):
+                qtbot.mouseClick(window.compute_button, QtCore.Qt.LeftButton)
+                assert window.eye_th.isRunning()
+
+def mock_init_cap(*args, **kwargs):
+    args[0].cap = cv2.VideoCapture("tests/dummy.jpg")
