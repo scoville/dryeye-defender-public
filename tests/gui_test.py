bd245e82a524002fa6be8fc4aa0f7596bdde2099
Praneil Kamat <pkamat@mac.com>
2023-05-19 10:26:48 +0100

    test(gui_test): test full custom QApplication instead of just MainWindow

--- tests/gui_test.py
+++ tests/gui_test.py
@@ -1,6 +1,8 @@
+# pylint: disable = redefined-outer-name, unused-argument
 """
 This is a unit test for running the application, and testing the detector on a few dummy frames.
 """
+from typing import Generator
 from unittest.mock import patch
 import pytest
 
@@ -9,7 +11,7 @@ import pytest_xvfb    # type: ignore[import]
 import cv2
 from PySide6.QtCore import Qt
 
-from eyeblink_gui.__main__ import MainWindow
+from eyeblink_gui.__main__ import Application
 from eyeblink_gui.widgets.window import Window
 from eyeblink_gui.widgets.eyeblink_model_thread import EyeblinkModelThread
 
@@ -24,23 +26,24 @@ def ensure_xvfb() -> None:
         raise Exception("Tests need Xvfb to run.")
 
 
+@pytest.fixture(scope="session")
+def qapp() -> Generator[Application, None, None]:
+    """Override creating a QApplication for the tests with our custom application."""
+    with patch("eyeblink_gui.widgets.window.EyeblinkModelThread.init_cap", new=mock_init_cap), \
+         patch("eyeblink_gui.widgets.window.get_cap_indexes", new=lambda *args, **kwargs: [0]):
+        yield Application([])
+
+
 def mock_init_cap(self: EyeblinkModelThread, input_device: int) -> None:
-    # pylint: disable = unused-argument
     """Mock the init_cap function to use a dummy image instead of a camera"""
     self.cap = cv2.VideoCapture(DUMMY_IMAGE_PATH)  # pylint: disable=no-member
 
 
-@patch("eyeblink_gui.widgets.window.DEBUG", True)
-def test_main_window(qtbot: QtBot) -> None:
-    """Test the main window, and the running the detector for a few frames on a dummy image"""
-    with patch("eyeblink_gui.widgets.window.EyeblinkModelThread.init_cap", new=mock_init_cap), \
-         patch("eyeblink_gui.widgets.window.get_cap_indexes", new=lambda *args, **kwargs: [0]):
-        main_window = MainWindow()
-        main_window.show()
-        qtbot.addWidget(main_window)
-        window = Window(main_window.centralWidget())
-        for _ in range(5):
-            with qtbot.waitSignal(window.eye_th.finished, timeout=EYEBLINK_MODEL_THREAD_TIMEOUT):
-                qtbot.mouseClick(window.compute_button, Qt.MouseButton.LeftButton)
-                assert window.eye_th.isRunning()
-            window.eye_th.cap = cv2.VideoCapture(DUMMY_IMAGE_PATH)  # pylint: disable=no-member
+def test_application(qtbot: QtBot, qapp: Application) -> None:
+    """Test the main application, running the detector for a few frames on a dummy image"""
+    window = Window(qapp.main_window.centralWidget())
+    for _ in range(5):
+        with qtbot.waitSignal(window.eye_th.finished, timeout=EYEBLINK_MODEL_THREAD_TIMEOUT):
+            qtbot.mouseClick(window.compute_button, Qt.MouseButton.LeftButton)
+            assert window.eye_th.isRunning()
+        window.eye_th.cap = cv2.VideoCapture(DUMMY_IMAGE_PATH)  # pylint: disable=no-member
