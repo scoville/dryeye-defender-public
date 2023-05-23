45fdd5bf94fe296627514dead81431e786e1619f
bml1g12 <ben.lowe.uk@googlemail.com>
2023-05-23 16:24:20 +0100

    docs(relaaser): fix docstring

--- releaser.sh
+++ releaser.sh
@@ -5,7 +5,7 @@ set -Eeuxo pipefail
 
 # 1. Clone or pip install https://github.com/scoville/github-release-tool and put release.py into their path
 # 2. Ensure they have created and checkout out a branch called "release/${version}" (i.e. a staging branch) with the code for the next release
-# 3. ./releaser.sh ${version} will update setup.py with latest version and commit it, then perform the git flow operations needed to automate the release
+# 3. ./releaser.sh ${version} will update setup.py, pyproject.toml and deb_build/DEBIAN/control with latest version and commit it, then perform the git flow operations needed to automate the release
 # 4. The ci will build on that tag and deploy the Github release
 
 VERSION=$1
