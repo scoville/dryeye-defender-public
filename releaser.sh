3e610f7feb8ba087065c945bedd55d3e29b94c59
bml1g12 <ben.lowe.uk@googlemail.com>
2023-05-23 16:23:18 +0100

    docs(releaser): typo

--- releaser.sh
+++ releaser.sh
@@ -4,7 +4,7 @@ set -Eeuxo pipefail
 # The person performing the release would do the following:
 
 # 1. Clone or pip install https://github.com/scoville/github-release-tool and put release.py into their path
-# 2. Ensure they have created and checkout out a branch called "release/${version}" (i.e. a staging branch) with the code for the next srelease
+# 2. Ensure they have created and checkout out a branch called "release/${version}" (i.e. a staging branch) with the code for the next release
 # 3. ./releaser.sh ${version} will update setup.py with latest version and commit it, then perform the git flow operations needed to automate the release
 # 4. The ci will build on that tag and deploy the Github release
 
