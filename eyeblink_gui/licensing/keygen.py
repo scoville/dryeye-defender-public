7bb79cf57bc6e5f2e5207159d2cd8606c3910829
Praneil Kamat <pkamat@mac.com>
2023-06-21 13:50:13 +0100

    feat(licensing/keygen): add license key generation and validation

---
+++ eyeblink_gui/licensing/keygen.py
@@ -0,0 +1,42 @@
+import base64
+
+import ecdsa
+
+
+def generate_key(data, signing_key):
+    """Generate a license key from a signing key
+
+    :param data: the data to sign
+    :param signing_key: the signing key
+    :return: the license key
+    """
+    signature = signing_key.sign(data)
+
+    data_encoded = base64.b64encode(data).decode()
+    signature_encoded = base64.b64encode(signature).decode()
+
+    return f"{data_encoded}.{signature_encoded}"
+
+
+def validate_key(license_key, verifying_key):
+    """Validate a license key
+
+    :param license_key: the license key
+    :param verifying_key: the verifying key
+    :return: True if the license key is valid, False otherwise
+    """
+    data, signature = license_key.split(".")
+    data = base64.b64decode(data)
+    signature = base64.b64decode(signature)
+
+    return verifying_key.verify(signature, data)
+
+
+sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) 
+vk = sk.get_verifying_key()
+
+license_key = generate_key(b"hello world", sk)
+print(f"{license_key}")
+
+isSignatureValid = validate_key(license_key, vk)
+print(f"{isSignatureValid=}")
