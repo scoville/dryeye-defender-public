9d7685f942b1647d8e1ee582d2b8d4735148cacf
Praneil Kamat <pkamat@mac.com>
2023-06-21 14:32:43 +0100

    feat(licensing/keygen): save and load public and private keys to/from string

--- eyeblink_gui/licensing/keygen.py
+++ eyeblink_gui/licensing/keygen.py
@@ -7,7 +7,7 @@ import ecdsa
 LOGGER = logging.getLogger(__name__)
 
 
-def generate_key(data, signing_key):
+def generate_key(data: bytes, signing_key: ecdsa.SigningKey):
     """Generate a license key from a signing key
 
     :param data: the data to sign
@@ -22,7 +22,7 @@ def generate_key(data, signing_key):
     return f"{data_encoded}.{signature_encoded}"
 
 
-def validate_key(license_key, verifying_key):
+def validate_key(license_key: str, verifying_key: ecdsa.VerifyingKey):
     """Validate a license key
 
     :param license_key: the license key
@@ -39,12 +39,25 @@ def validate_key(license_key, verifying_key):
         return False
 
 
-# TODO: export these keys to files
 sk = ecdsa.SigningKey.generate(curve=ecdsa.Ed25519) 
 vk = sk.get_verifying_key()
 
+sk_string = sk.to_string().hex()
+vk_string = vk.to_string().hex()
+
+# TODO: export signing key and verifying key to files
+
+# TODO: read signing key from file
+
+sk = ecdsa.SigningKey.from_string(bytes.fromhex(sk_string), curve=ecdsa.Ed25519)
 license_key = generate_key(b"hello@world.com", sk)
 print(f"{license_key}")
 
+# TODO: read verifying key from file
+
+vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(vk_string), curve=ecdsa.Ed25519)
 isSignatureValid = validate_key(license_key, vk)
 print(f"{isSignatureValid=}")
+
+# TODO: we should use an automatic obfuscator to obfuscate the code and protect against software cracking
+# See: https://piprogramming.org/articles/Ultimate-Introduction-to-cracking-Software-and-how-to-protect-against-it--Reverse-Engineering-0000000022.html
