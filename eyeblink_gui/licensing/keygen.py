9dbc1a7b87d181b0e012a9a33e468f9316c5e48e
Praneil Kamat <pkamat@mac.com>
2023-06-27 23:20:30 +0100

    feat(licensing/leygen): add parser for generating license keys

--- eyeblink_gui/licensing/keygen.py
+++ eyeblink_gui/licensing/keygen.py
@@ -1,20 +1,32 @@
+"""Functions to generate and validate license keys"""
+# TODO: we should use an automatic obfuscator to obfuscate the code and protect against software cracking
+# See: https://piprogramming.org/articles/Ultimate-Introduction-to-cracking-Software-and-how-to-protect-against-it--Reverse-Engineering-0000000022.html
+
 import logging
 import traceback
 import base64
+from argparse import ArgumentParser
 
 import ecdsa
 
 LOGGER = logging.getLogger(__name__)
 
+# Verifying key
+VERIFYING_KEY_STRING = "e3c77e79b005eb55b139b3d6a09b36364bf390071e0ac2b43f897d3ec769d4a7"
 
-def generate_key(data: bytes, signing_key: ecdsa.SigningKey):
+
+def generate_license_key(user_email: str, order_num: int, signing_key: str):
     """Generate a license key from a signing key
 
-    :param data: the data to sign
+    :param user_email: the user email
+    :param order_num: the order number
     :param signing_key: the signing key
     :return: the license key
+
     """
-    signature = signing_key.sign(data)
+    sk = ecdsa.SigningKey.from_string(bytes.fromhex(signing_key), curve=ecdsa.Ed25519)
+    data = f"{user_email}.{order_num}".encode()
+    signature = sk.sign(data)
 
     data_encoded = base64.b64encode(data).decode()
     signature_encoded = base64.b64encode(signature).decode()
@@ -22,42 +34,61 @@ def generate_key(data: bytes, signing_key: ecdsa.SigningKey):
     return f"{data_encoded}.{signature_encoded}"
 
 
-def validate_key(license_key: str, verifying_key: ecdsa.VerifyingKey):
+def validate_license_key(license_key: str, verifying_key: ecdsa.VerifyingKey=VERIFYING_KEY_STRING):
     """Validate a license key
 
     :param license_key: the license key
     :param verifying_key: the verifying key
-    :return: True if the license key is valid, False otherwise
+    :return: True if the license key is valid, False otherwise or if an error occurred
     """
     try:
+        vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(verifying_key), curve=ecdsa.Ed25519)
         data, signature = license_key.split(".")
         data = base64.b64decode(data)
         signature = base64.b64decode(signature)
-        return verifying_key.verify(signature, data)
+        return vk.verify(signature, data)
     except Exception:
         LOGGER.warning("Handled the following user error:\n%s", traceback.format_exc())
         return False
 
 
-sk = ecdsa.SigningKey.generate(curve=ecdsa.Ed25519) 
-vk = sk.get_verifying_key()
+def generate_sk_and_vk():
+    """Generate a signing key and a verifying key
 
-sk_string = sk.to_string().hex()
-vk_string = vk.to_string().hex()
+    :return: a tuple containing the signing key and the verifying key
+    """
+    sk = ecdsa.SigningKey.generate(curve=ecdsa.Ed25519)
+    vk = sk.get_verifying_key()
+    return sk.hex(), vk.hex()
 
-# TODO: export signing key and verifying key to files
 
-# TODO: read signing key from file
+def parse_args_and_generate_license_key():
+    """Parses arguments and generates a license key.
+    """
+    parser = ArgumentParser("Generate a license key")
+    parser.add_argument("--user_email",
+                        "-ue",
+                        type=str,
+                        required=True,
+                        help="email of the user")
+    parser.add_argument("--order_num",
+                        "-on",
+                        type=int,
+                        required=True,
+                        help="order number")
+    parser.add_argument("--signing_key",
+                        "-sk",
+                        type=str,
+                        required=True,
+                        help="file path of the signing key")
 
-sk = ecdsa.SigningKey.from_string(bytes.fromhex(sk_string), curve=ecdsa.Ed25519)
-license_key = generate_key(b"hello@world.com", sk)
-print(f"{license_key}")
+    args = parser.parse_args()
 
-# TODO: read verifying key from file
+    with open(args.signing_key, "r") as f:
+        sk = args.signing_key = f.read()
+        license_key = generate_license_key(args.user_email, args.order_num, args.signing_key)
+    return license_key
 
-vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(vk_string), curve=ecdsa.Ed25519)
-isSignatureValid = validate_key(license_key, vk)
-print(f"{isSignatureValid=}")
 
-# TODO: we should use an automatic obfuscator to obfuscate the code and protect against software cracking
-# See: https://piprogramming.org/articles/Ultimate-Introduction-to-cracking-Software-and-how-to-protect-against-it--Reverse-Engineering-0000000022.html
+if __name__ == "__main__":
+    parse_args_and_generate_license_key()
