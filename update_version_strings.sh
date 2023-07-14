789e8090040e124c046ab30d052075850a8b1970
bml1g12 <ben.lowe.uk@googlemail.com>
2023-07-14 13:41:35 +0100

    fix(osx-continous-deployment): sed command in OSX needs a suffix on inplace operations

--- update_version_strings.sh
+++ update_version_strings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 VERSION=$1
-sed -i "s/version =.*/version = \"${VERSION}\"/g" pyproject.toml
-sed -i "s/Version:.*/Version: ${VERSION}/g" deb_build/DEBIAN/control
-sed -i "s/'CFBundleShortVersionString': '.*'/'CFBundleShortVersionString': '${VERSION}'/g" pyinstaller_build.spec
-sed -i "s/version='.*'/version='${VERSION}'/g" pyinstaller_build.spec
+sed -i.bak "s/version =.*/version = \"${VERSION}\"/g" pyproject.toml
+sed -i.bak "s/Version:.*/Version: ${VERSION}/g" deb_build/DEBIAN/control
+sed -i.bak "s/'CFBundleShortVersionString': '.*'/'CFBundleShortVersionString': '${VERSION}'/g" pyinstaller_build.spec
+sed -i.bak "s/version='.*'/version='${VERSION}'/g" pyinstaller_build.spec
