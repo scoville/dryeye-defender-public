ff1d29a6a2f8acefa2d7adb0784690da4be9c757
bml1g12 <ben.lowe.uk@googlemail.com>
2023-08-25 08:47:09 +0100

    refactor(tests): linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,10 @@
 import os
 import pytest
 
+
 @pytest.fixture(scope="session", autouse=True)
-def set_environment_vars():
+def set_environment_vars() -> None:
     """Set an environment variable for all tests to notify the get_saved_data_path()
-    function to return /tmp/saved_blink.db instead of the real database path."""
+    function to return /tmp/saved_blink.db instead of the real database path.
+    """
     os.environ["CI_TESTS"] = "True"
