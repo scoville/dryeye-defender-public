c8e1c19e323755a4aba087b902fd09648eb47c61
bml1g12 <ben.lowe.uk@googlemail.com>
2023-11-03 19:22:36 +0000

    tests(manual/soundeffect): add manual test for sound effect playing

--- tests/manual/soundeffect.py
+++ tests/manual/soundeffect.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtMultimedia import QSoundEffect
@@ -7,11 +7,12 @@ from PySide6.QtMultimedia import QSoundEffect
 
 def main():
     app = QGuiApplication(sys.argv)
-    filename = "images/audiocheck.net_sin_800Hz_-3dBFS_0.19s.wav"
+    filename = "assets/audiocheck.net_sin_800Hz_-3dBFS_0.19s.wav"
     effect = QSoundEffect()
     effect.setSource(QUrl.fromLocalFile(filename))
     effect.setLoopCount(1)  # play once
     effect.play()
+    time.sleep(2)
     sys.exit(app.exec())
 
 
