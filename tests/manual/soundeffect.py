0cba0c7a24b4357f6366d1bc8d6edcbb33f8240c
bml1g12 <ben.lowe.uk@googlemail.com>
2023-11-08 14:14:13 +0000

    tests: add attempt to use pyside6 sound widgets, but they just don't work

--- tests/manual/soundeffect.py
+++ tests/manual/soundeffect.py
@@ -1,23 +1,66 @@
 """Demo of how to play sound effects"""
 import sys
 import time
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtMultimedia import QSoundEffect
+from PySide6.QtCore import QUrl, QCoreApplication
+from PySide6.QtMultimedia import QSoundEffect, QMediaDevices, QAudioDevice
+
+
+def get_default_sound_device() -> QAudioDevice:
+    devices = QMediaDevices.audioOutputs()
+    device_list = {}
+    number = 1
+    for device in devices:
+        default = " "
+        device_list[str(number)] = QAudioDevice(device)
+        if device.isDefault():
+            default = "#"
+            default_device_id = number
+        print(f"{default} {number} - {device.description()}")
+        number += 1
+    audioDevice = device_list[str(default_device_id)]
+    print(f"Audio device id {audioDevice.id()}")
+    assert audioDevice.isDefault(), f"isDefaultAudio device: {audioDevice.isDefault()}"
+    return audioDevice
+
+
+effect = None
 
 
 def main() -> None:
     """Demo of sound effect usage"""
-    app = QGuiApplication(sys.argv)
+    app = QCoreApplication(sys.argv)
+
     filename = "assets/audiocheck.net_sin_800Hz_-3dBFS_0.19s.wav"
 
-    filename = "assets/file_example_WAV_1MG.wav"
-    effect = QSoundEffect()
+    # filename = "assets/file_example_WAV_1MG.wav"
+    global effect
+    effect = QSoundEffect(get_default_sound_device())
+    # effect.setParent(app)
+    effect.loadedChanged.connect(
+        lambda: print("loadedChanged Changed", effect.status().name)
+    )
+    effect.sourceChanged.connect(lambda: print("sourceChanged Changed"))
+    effect.statusChanged.connect(lambda: print("mutedChanged Changed"))
+    effect.volumeChanged.connect(lambda: print("volumeChanged Changed"))
+    effect.loopsRemainingChanged.connect(lambda: print("loopsRemainingChanged Changed"))
+    effect.playingChanged.connect(lambda: print("playingChanged Changed"))
+    effect.audioDeviceChanged.connect(lambda: print("audioDeviceChanged Changed"))
     effect.setSource(QUrl.fromLocalFile(filename))
     effect.setLoopCount(1)  # play once
-    time.sleep(2)  # wait for it to load
+
+    # while effect.status().name == "Loading":
+    #     time.sleep(0.2)
+    #     print(effect.status().name)
+
+    print(effect.status().name)
+    time.sleep(2)
     effect.play()
-    print(f"effect status: {effect.status()}")
+    print(effect.status().name)
+    print("here")
+    time.sleep(2)
+    effect.play()
+    print("here2")
+    print(effect.status().name)
     sys.exit(app.exec())
 
 
